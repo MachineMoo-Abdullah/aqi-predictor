cecf3a6b210d2ff3c99f2585caa9b41c8b4e7b81
MachineMoo-Abdullah <abdullahprivate1@gmail.com>
2026-09-01 12:56:28 +0500

    initial commit

--- src/model/pipeline.py
+++ src/model/pipeline.py
@@ -1,6 +1,6 @@
-from dl_train_model import train_model
-from incremental_training import incremental_train
-from evaluate_model import evaluate_model
+from .dl_train_model import train_model
+from .incremental_training import incremental_train
+from .evaluate_model import evaluate_model
 
 
 def initial_pipeline():
